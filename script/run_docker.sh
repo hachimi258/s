87467ff397fc457ff9f1df3499591de1e069465c
zengzebin <zengzebin@lejurobot.com>
2025-06-03 20:10:22 +0800

    feat:挂载一个存放cppad缓存的目录进容器，避免每次都重新编译cppad

--- script/run_docker.sh
+++ script/run_docker.sh
@@ -4,11 +4,14 @@ PROJECT_DIR=$(realpath "$SCRIPT_DIR/../")
 
 echo $PROJECT_DIR
 
+mkdir -p "$PROJECT_DIR/.cppad"
+
 xhost +
 docker run --name kuavo-tv -it --runtime=nvidia --gpus all -e "ACCEPT_EULA=Y" --rm --network=host \
     --ulimit rtprio=99 \
     -e "PRIVACY_CONSENT=Y" \
     -v $HOME/.Xauthority:/root/.Xauthority \
+    -v "$PROJECT_DIR/.cppad:/var/ocs2" \
     -e DISPLAY \
     -e NVIDIA_VISIBLE_DEVICES=all \
     -e NVIDIA_DRIVER_CAPABILITIES=all,display \
